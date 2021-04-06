08391ede62fb0a702f022c23930fed8d60bc566e
Alexander Schulte <alex11.schulte@googlemail.com>
2021-04-06 16:59:33 +0200

    Add useEffect with initial Count to getItem + save the count with SetItem

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,6 +3,16 @@ import { useEffect, useState } from "react";
 function Counter() {
   const [count, setCount] = useState(0);
   // Your code below
+  useEffect(() => {
+    console.log("get data from local storage");
+    const dataFromLocalStorage = localStorage.getItem("count") ?? "";
+    setCount(Number(dataFromLocalStorage));
+  }, []);
+
+  useEffect(() => {
+    console.log("saved the current counter");
+    localStorage.setItem("count", count);
+  }, [count]);
 
   function handleDecrement() {
     setCount(count - 1);
