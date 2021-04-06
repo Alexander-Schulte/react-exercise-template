03edc00484407c5d4ba027b102b8f959bc82e673
Alexander Schulte <alex11.schulte@googlemail.com>
2021-04-06 16:27:33 +0200

    Add useEffect and the function handleWindowWidth to add/remove the EventListener

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ function App() {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
   // Your code below
 
+  useEffect(() => {
+    function handleWindowWidth() {
+      setWindowWidth(window.innerWidth);
+    }
+
+    console.log("On first render");
+    window.addEventListener("resize", handleWindowWidth);
+    return () => {
+      window.removeEventListener("resize", handleWindowWidth);
+    };
+  }, []);
+
   return (
     <div className="App">
       <p>Window Width: {windowWidth} </p>
